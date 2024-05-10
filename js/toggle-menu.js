9f75cb5ea3ac7f5fff65d6f672cc1a1ef8537f6b
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-05-10 11:33:35 +0800

    Header, Input search, navbar

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,18 +1,28 @@
-const menuClosebutton = document.querySelector('.navbar__toggle-menu-button-close');
-const menuOpenbutton = document.querySelector('.navbar__toggle-menu-button-open');
+const menuCloseButton = document.querySelector('.navbar__toggle-menu-button-close');
+const menuOpenButton = document.querySelector('.navbar__toggle-menu-button-open');
+const searchInputOpenButton = document.querySelector('.navbar__toggle-search-input-button');
+const searchInputCloseButton = document.querySelector('.navbar__close-search-input-button');
 
-menuClosebutton.addEventListener('click', () => {
+menuCloseButton.addEventListener('click', () => {
   const navbarItemsContainer = document.querySelector('.navbar__items-container');
-  const backdrop = document.querySelector('.backdrop');
 
   navbarItemsContainer.classList.remove('navbar__items-container--open');
-  backdrop.classList.remove('backdrop--open');
 });
 
-menuOpenbutton.addEventListener('click', () => {
+menuOpenButton.addEventListener('click', () => {
   const navbarItemsContainer = document.querySelector('.navbar__items-container');
-  const backdrop = document.querySelector('.backdrop');
 
   navbarItemsContainer.classList.add('navbar__items-container--open');
-  backdrop.classList.add('backdrop--open');
+});
+
+searchInputOpenButton.addEventListener('click', () => {
+  const navbarInputSearch = document.querySelector('.navbar__input-search');
+
+  navbarInputSearch.classList.add('navbar__input-search--open');
+});
+
+searchInputCloseButton.addEventListener('click', () => {
+  const navbarInputSearch = document.querySelector('.navbar__input-search');
+
+  navbarInputSearch.classList.remove('navbar__input-search--open');
 });
