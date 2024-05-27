52623bf942f124317375bef9caec52a5c076eb1d
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-05-27 15:43:35 +0800

    FQA improvement

--- js/fqa.js
+++ js/fqa.js
@@ -1,5 +1,6 @@
 [...document.querySelectorAll('.fqa__item-question-icon')].forEach((collapseButton) => 
-  collapseButton.addEventListener('click', ({target}) => 
-    target.closest('.fqa__item') && 
-      target.closest('.fqa__item').classList.toggle('fqa__item--open')
-));
+  collapseButton.addEventListener('click', ({target}) => {
+    document.querySelector('.fqa__item--open')?.classList.remove('fqa__item--open');
+    target.closest('.fqa__item') && target.closest('.fqa__item').classList.toggle('fqa__item--open')
+  })
+);
