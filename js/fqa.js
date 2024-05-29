3fc09d98207361a1a76f7a6e671228409fc4a112
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-05-29 09:59:11 +0800

    About us page

--- js/fqa.js
+++ js/fqa.js
@@ -1,6 +1,8 @@
 [...document.querySelectorAll('.fqa__item')].forEach((item) => 
   item.addEventListener('click', ({target}) => {
+    
     document.querySelector('.fqa__item--open')?.classList.remove('fqa__item--open');
-    target.closest('.fqa__item') && target.closest('.fqa__item').classList.toggle('fqa__item--open')
+
+    target.closest('.fqa__item') && target.closest('.fqa__item').classList.toggle('fqa__item--open');
   })
 );
